4c202340a58f5d4a86059009e8d251d6eb51fe8b
Robby Lewis <Robbysim37@gmail.com>
2022-11-24 10:35:10 -0500

    started implementing browser routing

--- robert-lewis-website/src/App.js
+++ robert-lewis-website/src/App.js
@@ -1,8 +1,18 @@
 import Header from "./components/Header"
 import './App.css';
+import Bodytest from "./components/Bodytest";
+import {Route,Routes} from "react-router-dom"
+
 
 function App() {
-  return <Header></Header>
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/test" element={<Bodytest></Bodytest>}></Route>
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
